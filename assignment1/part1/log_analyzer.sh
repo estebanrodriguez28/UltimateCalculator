97148c57904c796c36dd442f49670c7da1d3397d
Kevin Grogan <kevin.p.grogan@gmail.com>
2023-09-09 13:09:43 -0700

    Scaffold assignment 1 part 1

---
+++ assignment1/part1/log_analyzer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# The first two characters in the above line is called the shebang (#!). 
+# It tells the OS what program to use to run the subsequent script.
+# For this assignment, we are using bash; however, you could also point to other interpreters such as zsh or even python (don't do this).
+# It may be the case that you want to use a bash program that is in a different location (e.g., a newer version than one shipped with your OS).
+# For this, you just need to change the path after the shebang.
+
+# YOUR CODE GOES HERE
