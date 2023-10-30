09089cf16a22a6f542d88aa33651e4bed4659846
Dillon Timmer <dtimmer@sandiego.edu>
2023-10-29 19:30:23 -0700

    library add and find methods

--- assignment3/part1/src/main/java/usd/Library.java
+++ assignment3/part1/src/main/java/usd/Library.java
@@ -1,19 +1,60 @@
 package usd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    // TODO: add member data
 
-    public void addBook(Book book){
-        //TODO: Implement adding a book to the library
+    private ArrayList<Book> library;
+
+    public Library() {
+        this.library = new ArrayList<>();
     }
 
-    public List<Book> find(String[] bookAttributes){
-        //TODO: Implement finding the books that match all attributes.
-        // Each book attribute will be in the form "<ATTRIBUTE_NAME>:<ATTRIBUTE>"
-        // and an IllegalArgumentException will be thrown if the <ATTRIBUTE_NAME> is
-        // invalid.
-        return null;
+    public void addBook(Book book) {
+        this.library.add(book);
+    }
+
+    public List<Book> find(String[] bookAttributes) {
+        ArrayList<Book> findList = new ArrayList<>();
+
+        // Throw error if more than four arguments
+        if (bookAttributes.length > 4) {throw new IllegalArgumentException("Invalid number of attributes"); }
+
+        // Iterate over all books and inputs
+        for (int i = 0; i < library.size(); i++) {
+
+            Boolean valid = true;
+
+            for (int j = 0; j < bookAttributes.length; j++) {
+
+                // Split input into parts
+                String attributeName = (bookAttributes[i].split(':'))[0];
+                String attribute = (bookAttributes[i].split(':'))[1];
+
+                // Switch statement over input to determine if book is to be included in results
+                switch (attributeName) {
+                    case "title":
+                        if (!library.get(i).getTitle().equals(attribute)) { valid = false; }
+                        break;
+                    case "author":
+                        if (!library.get(i).getAuthor().equals(attribute)) { valid = false; }
+                        break;
+                    case "isbn":
+                        if (!library.get(i).getIsbn().equals(attribute)) { valid = false; }
+                        break;
+                    case "shelfLocation":
+                        if (!library.get(i).getShelfLocation().equals(attribute)) { valid = false; }
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid attribute");
+                }
+            }
+
+            // Add to results if book is valid
+            if (valid == true) { findList.add(library.get(i)); }
+
+        }
+
     }
 }
