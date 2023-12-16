3da77b5b16ab528a48744850205fab555f59e50f
Sara Evans <saraevans@sandiego.edu>
2023-12-16 14:06:06 -0800

    finance ui

---
+++ finalproject/src/main/java/edu/usd/FinanceService/FinancialCalculator.java
@@ -0,0 +1,17 @@
+package com.example.financial;
+
+import java.util.List;
+public class FinancialCalculator extends Calculator {
+
+    private Finances finances;
+
+    public FinancialCalculator(Finances finances) {
+        this.finances = finances;
+    }
+
+    public List<Double> calculate() {
+        return finances.getCalculations();
+    }
+
+
+}
