8b87035bf3f532df593099b6c72af39fa93142ef
Dillon Timmer <dtimmer@sandiego.edu>
2023-12-17 18:28:24 -0800

    tests build

--- finalproject/src/test/java/edu/usd/FinanceService/FinancesTest.java
+++ finalproject/src/test/java/edu/usd/FinanceService/FinancesTest.java
@@ -1,3 +1,5 @@
+package edu.usd.FinanceService;
+
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
